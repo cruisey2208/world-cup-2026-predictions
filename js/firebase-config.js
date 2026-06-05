35bdbe5376b636dee374203a3ef0e5509dffda2a
cruisey2208 <cruisey2208@hotmail.com>
2026-06-05 21:52:54 +0100

    Initial project structure and Firebase config

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDN7kYZKAYRFL1fWWaHtgXXUHhRdbaFXLU",
+  authDomain: "nathans-wc-2026.firebaseapp.com",
+  projectId: "nathans-wc-2026",
+  storageBucket: "nathans-wc-2026.firebasestorage.app",
+  messagingSenderId: "868588354154",
+  appId: "1:868588354154:web:32015d8f11cbea99656813",
+  measurementId: "G-904CXEJ39R"
+};
